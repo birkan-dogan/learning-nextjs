5ecdcc19f36cdf45b563693d5bd841816c5f4d97
birkan <b.561birkan@gmail.com>
2022-10-25 21:22:51 +0300

    navigating to different routes by using Link comp.

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,7 +1,11 @@
+import Link from "next/link";
 const Blog = () => {
   return (
     <div>
       <h1>Blog page</h1>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
     </div>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -17,6 +18,12 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>I'm learning Next.js</p>
+        <Link href="/blog">
+          <a>Blog</a>
+        </Link>
+        <Link href="/product">
+          <a>Products</a>
+        </Link>
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
