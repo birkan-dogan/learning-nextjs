a8c18336c4731e45e0a4ab17fb20e673c3b6aab4
birkan <b.561birkan@gmail.com>
2022-10-26 16:39:11 +0300

    using getStaticProps function to fetch data

---
+++ pages/users.js
@@ -0,0 +1,32 @@
+const UserList = (props) => {
+  const { users } = props;
+  return (
+    <div>
+      <h1>List of users:</h1>
+      <div>
+        {users.map((user) => {
+          return (
+            <div key={user.id}>
+              <p>{user.name}</p>
+              <p>{user.email}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+
+export default UserList;
+
+export const getStaticProps = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+  return {
+    props: {
+      users: data,
+    },
+  };
+};
+
+// generating html after fetching data by using getStaticProps function
