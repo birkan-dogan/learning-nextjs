59965bba60f0bd08619b4d9c0b16063a286e12d2
birkan <b.561birkan@gmail.com>
2022-10-26 18:48:41 +0300

    fetching paths for getStaticPaths

--- pages/posts/[postId].js
+++ pages/posts/[postId].js
@@ -13,6 +13,20 @@ const Post = ({ post }) => {
 
 export default Post;
 
+export const getStaticPaths = async () => {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await response.json();
+  const paths = data.map((post) => {
+    return {
+      params: { postId: `${post.id}` },
+    };
+  });
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
 export const getStaticProps = async (context) => {
   const { params } = context;
   const response = await fetch(
