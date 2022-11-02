0cbc69fbeea2cca9803a844d5be792ed8c74cbd4
birkan <b.561birkan@gmail.com>
2022-11-02 17:26:48 +0300

    closer look at Incremental Static Generation

--- pages/users.js
+++ pages/users.js
@@ -27,7 +27,27 @@ export const getStaticProps = async () => {
     props: {
       users: data,
     },
+    revalidate: 60,
   };
 };
 
 // generating html after fetching data by using getStaticProps function
+
+/*
+using Incremental Static re-Generation (ISR)
+
+what if we have data that changes frequently ?
+
+Next.js has a built-in feature which is called incremental static generation. It means that we don't just generate our page statically once at build time, but it's continuously updated even after deployment without us re-deploying it.
+
+We have a pre-generated page, but then we can also tell Next.js that a given page should be re-generated again for every incoming request at most every x seconds.
+
+So, we have ongoing pre-rendering on the server for incoming requests.
+And all we need to do to unlock this is in the object, which we return in getStaticProps function.
+
+revalidate: time in seconds that Next.js should wait until it re-generates this page.
+
+during development, the page will be re-generated for every request, no matter what we enter revalidate value. (getStaticProps function always run again)
+But in production, this number will matter.
+
+*/
