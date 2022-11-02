a4d66380d0c19845a449b516514118b1906ab3fa
birkan <b.561birkan@gmail.com>
2022-11-02 23:58:59 +0300

    fallback:true

--- pages/repeat/[productId].js
+++ pages/repeat/[productId].js
@@ -3,6 +3,12 @@ import path from "path";
 
 const ProductDetailPage = (props) => {
   const { loadedProduct } = props;
+
+  if (!loadedProduct) {
+    // we're waiting for the product to load
+    return <p>Loading...</p>;
+  }
+
   return (
     <div>
       <h1>{loadedProduct.title}</h1>
@@ -28,6 +34,12 @@ export const getStaticProps = async (context) => {
   const data = await getData();
 
   const product = data.products.find((product) => product.id === productId);
+
+  if (!product) {
+    // if we don't have a product, it will return 404 page
+    return { notFound: true };
+  }
+
   return {
     props: {
       loadedProduct: product,
@@ -45,6 +57,19 @@ export const getStaticPaths = async () => {
   });
   return {
     paths,
-    fallback: false,
+    fallback: true,
   };
 };
+
+/*
+what's up with the fallback key?
+The fallback key can help us if we have a lot of pages that would need to be pre-generated
+
+--> fallback:true
+we tell Next.js that even pages which are not listed in params, can be valid values that should be loaded when they are visited. But, they're not pre-generated, instead they're generated just in time when a request reaches the server. And that allows us to pre-generate highly visited pages, and delay the generation less frequented pages, so they are only pre-generated when they're needed.
+
+--> fallback:false
+
+--> fallback:"blocking"
+
+*/
